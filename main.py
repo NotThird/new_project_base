95f06cc0b557f36fc4493ca832f4a314cd8b4589
NotThird <129929764+NotThird@users.noreply.github.com>
2023-06-11 02:10:31 -0500

    Add auto-clicking feature

--- main.py
+++ main.py
@@ -8,13 +8,19 @@ clock = pygame.time.Clock()
 
 running = True
 score = 0
+autoclick_counter = 0
 
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT: 
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             score += 1
+    
+    autoclick_counter += 1
+    if autoclick_counter >= 6000: # auto-click every 10 seconds
+        score += 1
+        autoclick_counter = 0
     
     screen.fill((0, 0, 0))
     font = pygame.font.Font(None, 50)
