8c9c6c4bd28dbca14d9d345d2a24348b567b17b4
NotThird <129929764+NotThird@users.noreply.github.com>
2023-06-11 02:08:05 -0500

    Add clicker feature

--- main.py
+++ main.py
@@ -7,13 +7,19 @@ screen = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
 
 running = True
+score = 0
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            score += 1
     
     screen.fill((0, 0, 0))
+    font = pygame.font.Font(None, 50)
+    text = font.render(str(score), True, (255, 255, 255))
+    screen.blit(text, (350, 275))
     pygame.display.update()
     clock.tick(60)
 
